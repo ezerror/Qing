5b1afd835277cf951f8ed30ec3868245786182a6
ezerror <435719422@qq.com>
2023-03-07 23:04:45 +0800

    生成类文件以及类中的方法 todo: 1、目前的方法参数固定为了([Ljava/lang/String;)V需要按照语法结构获取 2、类中的语句还未实现

---
+++ compiler/src/main/java/me/ezerror/parsing/visitor/StatementVisitor.java
@@ -0,0 +1,13 @@
+package me.ezerror.parsing.visitor;
+
+import me.ezerror.QingBaseVisitor;
+import me.ezerror.domain.MethodDeclaration;
+
+/**
+ * @author ：ezerror
+ * @date ：Created in 2023/3/7 0:27
+ */
+public class StatementVisitor extends QingBaseVisitor<MethodDeclaration> {
+  
+  
+}
